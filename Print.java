f4f6a685a1bde3ee2a75503e11147cdbbaae567c
CoderMuMu <2820874381@qq.com>
2025-06-30 09:52:03 +0800

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,12 @@
+package huffmanCode;
+
+public class Print {
+    public void print(){
+        System.out.println("Enter your choice:");
+        System.out.println("1. Compress file");
+        System.out.println("2. Decompress file");
+        System.out.println("3. Exit");
+        System.out.println("------------------------");
+    }
+}
+
